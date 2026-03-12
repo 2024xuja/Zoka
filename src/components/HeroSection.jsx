200cbdf013f46ad1c62f994b6d4959d86f5dcdc7
Nazirov <nazirovxojaakbar@gmail.com>
2026-03-12 15:20:48 +0500

    Hero Sectio 20% has done

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,12 @@
 import { HiArrowRightCircle } from "react-icons/hi2";
-import Camera1 from '../assets/Camera1.png'
-import Camera2 from '../assets/Camera2.png'
-import Camera3 from '../assets/Camera3.png'
+import Camera1 from "../assets/Camera1.png";
+import Camera2 from "../assets/Camera2.png";
+import Camera3 from "../assets/Camera3.png";
 
 export default function HeroSection() {
   return (
     <section className="flex items-center justify-center gap-7 p-9">
-      <div>
+      <div className="">
         <h1 class="text-7xl font-black w-109.5 text-[#1A093C]  font-700 mb-7">
           Where creative process happens
         </h1>
@@ -15,16 +15,34 @@ export default function HeroSection() {
           Your creativity, our inspiration <br /> Whatever your story, set if
           free.
         </p>
-        <button className="relative p-3.5 px-12  bg-[#3F41A6] rounded-full text-white font-medium">
+        <button className="relative p-3.5 px-12  bg-[#3F41A6] rounded-full text-white font-medium shadow-2xl cursor-pointer">
           Get in Touch{" "}
         </button>
       </div>
       <div>
-            <img src={Camera1} alt="Camera rasmi" className="w-680px h-400px mb-5"/>
-            <div className="flex">
-            <img src={Camera2} alt="Bizning rasm" className="w-279px h-250px " />
-            <img src={Camera3} alt="Qiz" className="w-380px h-250px  pl-5.25" />
-            </div>
+        <img
+          src={Camera1}
+          alt="Camera rasmi"
+          className="w-680px h-400px mb-5"
+        />
+        <div className="flex mb-">
+          <img src={Camera2} alt="Bizning rasm" className="w-279px h-250px " />
+          <img src={Camera3} alt="Qiz" className="w-380px h-250px  pl-5.25" />
+        </div>
+      </div>
+      <div className="">
+        <h1 className="flex ">GET TO KNOW US</h1>
+        <h1>
+          Why we make it <br /> happens
+        </h1>
+        <p className="flex">
+          Your ceremony & reception venues, your <br /> vision, your dress, your
+          colours and anything <br /> else you would like.
+        </p>
+         <button className="relative p-3.5 px-12  bg-[#3F41A6] rounded-full text-white font-medium shadow-2xl cursor-pointer ">
+          Get in Touch{" "}
+        </button>
+        <input type="button" value="" /> 
       </div>
     </section>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ import { IoMdArrowDroprightCircle } from "react-icons/io";
 function Navbar() {
   return (
     <div className='flex items-center justify-evenly pt-5'>
-        <h1 className="font-bold size-36px">ZOKA</h1>
+        <h1 className="font-bold size-9px font-[Poppins]">ZOKA</h1>
           
        <ul className="flex items-center gap-8">
-          <Link to={Navbar}>Home</Link>
-          <Link >Pages</Link>
-          <Link >Photograpy</Link>
-          <Link >Photoshoots</Link>
+          <Link className="font-bold" to={Navbar}>Home</Link>
+          <Link className="font-bold">Pages</Link>
+          <Link className="font-bold">Photograpy</Link>
+          <Link className="font-bold">Photoshoots</Link>
        </ul>
-       <button className="flex border-4 w-36.5 h-57px rounded-3xl text-[#3F41A6] items-center justify-center  gap-1">Youtube <IoMdArrowDroprightCircle size={20}/></button>
+       <button className="flex border-4 w-36.5 h-57px rounded-3xl text-[#3F41A6] items-center justify-center  font-extrabold ">Youtube <IoMdArrowDroprightCircle size={20}/></button>
     </div>
     
   )
